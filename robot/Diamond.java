f93652399b0c55067d480bdb3ccab751e1c47c70
sreethumu <thumusreecharan@gmail.com>
2025-08-19 20:47:42 +0000

    finished the left side of the diamond

--- robot/Diamond.java
+++ robot/Diamond.java
@@ -9,19 +9,114 @@ public class Diamond implements Directions{
         World.setVisible(true);// allows us to see the run output
         // the bigger the street, the farther north
         World.setSize(20,20);
-        World.setDelay(5);
+        World.setDelay(1);
 
 
         // The line below creates a Robot that we will refer to as rob
         // Find out what the numbers and direction do!
         // Put rob in a better location for your initials.
-        Robot rob = new Robot(15,2,South,10000);
-        Robot diam = new Robot(15,2,South,10000);
+        Robot rob = new Robot(6,2,South,10000);
+        Robot diam = new Robot(19,10,West,10000);
+        Robot diam1 = new Robot(19,10,East,10000);
         // Want a second robot?  No prob.  They are cheap :)
         //Robot dude = new Robot(7,5,West,9);
         // examples of commands you can invoke on a Robot
         // move one step in the direction it is facing
 
+
+        
+
+        //Diamond
+        diam.putBeeper();
+        diam.move();
+        diam.putBeeper();
+        diam.move();
+        diam.putBeeper();
+        diam.move();
+        diam.putBeeper();
+        diam.move();
+        diam.putBeeper();
+        diam.move();
+        diam.putBeeper();
+        diam.turnLeft();
+        diam.move();
+        diam.putBeeper();
+        diam.turnLeft();
+        diam.turnLeft();
+        diam.turnLeft();
+        diam.move();
+        diam.putBeeper();
+        diam.turnLeft();
+        diam.move();
+        diam.putBeeper();
+        diam.turnLeft();
+        diam.turnLeft();
+        diam.turnLeft();
+        diam.move();
+        diam.putBeeper();
+        diam.turnLeft();
+        diam.move();
+        diam.putBeeper();
+        diam.move();
+        diam.putBeeper();
+        diam.turnLeft();
+        diam.move();
+        diam.putBeeper();
+        //copy and paste this
+        diam.turnLeft();
+        diam.turnLeft();
+        diam.turnLeft();
+        diam.move();
+        diam.putBeeper();
+        diam.turnLeft();
+        diam.move();
+        diam.putBeeper();
+        //
+        diam.turnLeft();
+        diam.turnLeft();
+        diam.turnLeft();
+        diam.move();
+        diam.putBeeper();
+        diam.turnLeft();
+        diam.move();
+        diam.putBeeper();
+        diam.turnLeft();
+        diam.turnLeft();
+        diam.turnLeft();
+        diam.move();
+        diam.putBeeper();
+        diam.turnLeft();
+        diam.move();
+        diam.putBeeper();
+        diam.turnLeft();
+        diam.turnLeft();
+        diam.turnLeft();
+        diam.move();
+        diam.putBeeper();
+        diam.turnLeft();
+        diam.move();
+        diam.putBeeper();
+        diam.turnLeft();
+        diam.turnLeft();
+        diam.turnLeft();
+        diam.move();
+        diam.putBeeper();
+        diam.turnLeft();
+        diam.move();
+        diam.putBeeper();
+        // left side diamond
+        diam1.putBeeper();
+        diam1.move();
+        diam1.putBeeper();
+        diam1.move();
+        diam1.putBeeper();
+        diam1.move();
+        diam1.putBeeper();
+        diam1.move();
+        diam1.putBeeper();
+        diam1.move();
+        diam1.putBeeper();
+        
 
         //Square
         rob.putBeeper();
@@ -105,11 +200,5 @@ public class Diamond implements Directions{
         rob.turnLeft();
         rob.move();
         rob.putBeeper();
-
-        //Diamond
-
-        // done with the line, now on the curve
-        // rob.turnLeft();
-
     }
 }
