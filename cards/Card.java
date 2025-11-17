ce8eb8ae77d26ac808435d3ff418d9859395ea3c
sreethumu <thumusreecharan@gmail.com>
2025-11-17 21:25:37 +0000

    finished card.java

--- cards/Card.java
+++ cards/Card.java
@@ -1,6 +1,21 @@
 package cards;
-
 public class Card {
+    private int suite;
+    private int value;
 
+    private String[] SUITES = {"♦", "♣", "♥", "♠"};
+    private String[] VALUES = {"Ace", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"};
 
+    public Card(int suite, int value){
+        this.suite = suite;
+        this.value = value;
+    }
+    
+    public int getValue(){
+        return suite;
+    }
+
+    public String toString(){
+        return "" + SUITES[suite] + VALUES[value];
+    }
 }
--- cards/Deck.java
+++ cards/Deck.java
@@ -2,5 +2,17 @@ package cards;
 
 public class Deck {
 
+    private String[] deck = new String[51];
+
+    public Deck(){
+        for(int num=0; num<52; num++){   
+            for(int i=0; i<4; i++){
+                for(int j=0; j<=12; j++){
+                    Card newCard = new Card(i,j);
+                    deck[num] = newCard.toString();
+                }
+            }
+        }
+    }
 
 }
