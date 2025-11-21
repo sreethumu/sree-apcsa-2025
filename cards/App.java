b27b3a98016df4a6e9b147927efb623483b62ec1
sreethumu <thumusreecharan@gmail.com>
2025-11-21 21:56:42 +0000

    finished project

--- cards/App.java
+++ cards/App.java
@@ -4,16 +4,11 @@ public class App {
 
     public static void main(String[] args) {
 
-        //CardTable table = new CardTable();
+        CardTable table = new CardTable();
 
-        //table.startPlaying();
-        Deck deck = new Deck();
-        Hand hand = new Hand(8);
-        Card queenOfHearts = new Card(2, 11);
-        for(int i=0; i<8; i++){
-            hand.add(deck.draw());
-        }
-        System.out.println(hand.toString());
+        table.startPlaying();
+
+
         //hand.toString();
         /*deck.print(5);
         deck.draw();
--- cards/Hand.java
+++ cards/Hand.java
@@ -5,23 +5,22 @@ public class Hand {
     private int max;
     private int addCardNum = 0;
     private Card[] hand;
+    private int length;
 
     public Hand(int x){
         hand = new Card[x];
+        max = x;
     }
     
     
     public void add(Card x){
         hand[addCardNum] = x;
         addCardNum++;
+        length++;
     }
 
     public int length(){
-        int numCards = 0;
-        while(hand[numCards] != null){
-            numCards++;
-        }
-        return numCards;
+        return length;
     }
 
     public Card get(int x){
@@ -30,6 +29,7 @@ public class Hand {
 
     public Card remove(int indexOfCard){
         Card removedCard = hand[indexOfCard];
+        hand[indexOfCard] = null;
         for(int i = 0; i<max-1; i++){
             if(hand[i] == null){
                 if(i==max-1){
@@ -41,14 +41,15 @@ public class Hand {
                 }
             }
         }
-        Hand hand = new Hand(max);
+        //max--; 
+        length--;
         ///Add removing length
         return removedCard;
     }
 
     public String toString(){
         String handString = "";
-        for(int i = 0; i<max; i++){
+        for(int i = 0; i<length; i++){
             handString += "" + hand[i].toString();
         }
         return handString;
