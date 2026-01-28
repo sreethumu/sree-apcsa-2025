80217b4fbd207e3853adcb4124f134624cf1875f
sreethumu <thumusreecharan@gmail.com>
2026-01-28 05:46:03 +0000

    forgot to commit because I was working in a java compiler so I could test my code, it doesn't work yet I have to adjust it

--- sorting/SelectionSort.java
+++ sorting/SelectionSort.java
@@ -3,6 +3,22 @@ package sorting;
 public class SelectionSort implements Sorter {
 
     public void sort(int[] input) {
+        for(int i=0; i<input.length; i++){
+            int min=input[i];
+            int temp;
+            //find smallest value
+            for(int j=i; j<input.length; j++){
+                if(input[j]<min){
+                    min=input[j];
+                }
+            }
+            //shift the array
+            for(int k=0; k<input.length-1; k++){
+                input[k+1] = input[k];
+            }
+            input[0]=min;
+        }
+        System.out.println(input);
         System.out.println("Selection Sort!!!");
     }
 }
